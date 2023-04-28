d0fa3252df04a2a069b61b5bbe0d304546ed059b
Iftekhar Fahim <iftekharfahim@Iftekhars-MacBook-Air.local>
2023-04-28 09:18:25 +0600

    creating php file

---
+++ index.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Url Shortner</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+</head>
+<body>
+    <div class="wrapper">
+        <form action="#">
+            <input type="text" placeholder="Enter a url" required>
+            <i class="fa fa-link url-icon util util-link"></i>
+            <button>Shorten</button>
+        </form>
+        <div class="count">
+            <span>Total Links: <span>10</span> &  Total Clicks:<span>120</span> </span>
+            <a href="#"> Clear All</a>
+        </div>
+        <div class="urls-area">
+            <div class="title">
+                <li>Shorten URL</li>
+                <li>Original Link</li>
+                <li>Clicks</li>
+                <li>Action</li>
+            </div>
+           
+            <div class="data">
+                <li><a href="#">example.com</a></li>
+                <li>google.com</li>
+                <li>19</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            
+            <div class="data">
+                <li><a href="#">example.com</a></li>
+                <li>google.com</li>
+                <li>19</li>
+                <li><a href="#">Delete</a></li>
+            </div><div class="data">
+                <li><a href="#">example.com</a></li>
+                <li>google.com</li>
+                <li>19</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">example.com</a></li>
+                <li>google.com</li>
+                <li>19</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+            <div class="data">
+                <li><a href="#">example.com</a></li>
+                <li>google.com</li>
+                <li>19</li>
+                <li><a href="#">Delete</a></li>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
