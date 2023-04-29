f8dc0ce41b49502255c0b9c697e41e3129002904
Iftekhar Fahim <iftekharfahim@Iftekhars-MacBook-Air.local>
2023-04-30 02:11:07 +0600

    sql updated

--- url.sql
+++ url.sql
@@ -1,19 +1,50 @@
+-- phpMyAdmin SQL Dump
+-- version 4.9.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:8889
+-- Generation Time: Apr 29, 2023 at 08:09 PM
+-- Server version: 5.7.26
+-- PHP Version: 7.4.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
 SET time_zone = "+00:00";
+
+--
+-- Database: `urlshortener`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `url`
+--
 
 CREATE TABLE `url` (
   `id` int(11) NOT NULL,
-  `shorten_url` varchar(200) NOT NULL,
-  `full_url` varchar(1000) NOT NULL,
+  `shorten_url` varchar(30) NOT NULL,
+  `full_url` varchar(15555) NOT NULL,
   `clicks` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
+--
+-- Dumping data for table `url`
+--
+
+INSERT INTO `url` (`id`, `shorten_url`, `full_url`, `clicks`) VALUES
+(63, '5ef92', 'https://www.linkedin.com/feed/', 2);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `url`
+--
 ALTER TABLE `url`
   ADD PRIMARY KEY (`id`);
 
-ALTER TABLE `url`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
-COMMIT;
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
